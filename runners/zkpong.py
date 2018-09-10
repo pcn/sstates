b7882e6069d020a314675d73427aeaa97f805a1c
Peter C. Norton <spacey@spacey.org>
2018-09-10 14:05:39 -0400

    Will this improve the messages

--- runners/zkpong.py
+++ runners/zkpong.py
@@ -37,10 +37,10 @@ def srvr_3(minion_id, slack_webhook_url=None, **kwargs):
             # restart zookeeper
             remedy_result = client.cmd(minion_id, 'service.restart', ['zookeeper'])
             slackres = salt.utils.http.query(
-                url=slack_webhook_url, method='POST', data=json.dumps({"text": str(remedy_result)}))
+                url=slack_webhook_url, method='POST', data=json.dumps({"text": "Restarted zookeeper - result is `{}`".format(str(remedy_result)})))
             log.info("Sending a message to slack, result is: {}".format(slackres))
             slackres = salt.utils.http.query(
-                url=slack_webhook_url, method='POST', data=json.dumps({"text": "sleeping for 10 seconds to slow things down"}))
+                url=slack_webhook_url, method='POST', data=json.dumps({"text": "After remediating, sleeping for 10 seconds"}))
             log.info("Sending a message to slack, result is: {}".format(slackres))
             time.sleep(10)
             break  # Only act on one node per alert
