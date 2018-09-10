6b5c1fac63dede8fd2e8a12071626198266356b5
Peter C. Norton <spacey@spacey.org>
2018-09-10 13:56:06 -0400

    Figure out what's up

--- runners/zkpong.py
+++ runners/zkpong.py
@@ -27,21 +27,23 @@ def srvr_2(minion_id, slack_webhook_url=None, **kwargs):
     return (srvr_result,)
 
 def srvr_3(minion_id, slack_webhook_url=None, **kwargs):
-    """In the real world, we would add some paranoia so we don't restart the master"""
+    """In the real world, we would add some paranoia so we don't restart the leader"""
     client = salt.client.LocalClient()
     logging.info("Showing zk srvr status")
     srvr_result = client.cmd('zk*', 'zk.srvr', [])
     
     for minion_id, data in sorted(srvr_result.items()):
-        # Only act on one node per alert
         if not data['result']:
             # restart zookeeper
             remedy_result = client.cmd(minion_id, 'service.restart', ['zookeeper'])
-            salt.utils.http.query(url=slack_webhook_url, method='POST',
-                                  data=json.dumps({"text": remedy_result}))
-            salt.utils.http.query(url=slack_webhook_url, method='POST',
-                                  data=json.dumps({"text": "sleeping for 10 seconds to slow things down"}))
+            slackres = salt.utils.http.query(
+                url=slack_webhook_url, method='POST', data=json.dumps({"text": str(remedy_result)}))
+            log.info("Sending a message to slack, result is: {}".format(slackres))
+            slackres = salt.utils.http.query(
+                url=slack_webhook_url, method='POST', data=json.dumps({"text": "sleeping for 10 seconds to slow things down"}))
+            log.info("Sending a message to slack, result is: {}".format(slackres))
             time.sleep(10)
+            break  # Only act on one node per alert
     srvr_result2 = client.cmd('zk*', 'zk.srvr', [])
     return (srvr_result2,)
     
